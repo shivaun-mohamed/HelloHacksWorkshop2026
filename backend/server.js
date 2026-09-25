2a6a57f881a9781bfcd86d9e23ca99cb2459baf9
shivaun-mohamed <shivaunmohamed@gmail.com>
2026-09-24 20:30:45 -0700

    updated ui, paired backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
